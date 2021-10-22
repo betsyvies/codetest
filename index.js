6dd3971798514d46878d8da6fe3fc4b015bbf161
Betsy Vidal <ing.betsyvies@gmail.com>
2021-10-21 20:37:31 -0500

    feat: Add function parseIp

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const http = require('http');
 
+const parseIp = (req) =>
+  req.headers['x-forwarded-for'].split(',').shift()
+  || req.socket.remoteAddress
+
 const requestListener = function (req, res) {
   res.writeHead(200);
   console.log('Hola Mundo')
 
-  res.end('Finally done')
+  res.end(parseIp(req))
 };
 
 const server = http.createServer(requestListener);
