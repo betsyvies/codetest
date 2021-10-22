dd7803c0fc6c2943ce89f6fc55526130c09f2b3b
Betsy Vidal <ing.betsyvies@gmail.com>
2021-10-21 20:44:25 -0500

    feat: Add function requestHandled

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const http = require('http');
 
-const parseIp = (req) =>
-  req.headers['x-forwarded-for'].split(',').shift()
-  || req.socket.remoteAddress
+let ipAddressReqs = new Map();
+
+const parseIp = (req) => req.headers['x-forwarded-for'].split(',').shift()
+  || req.socket.remoteAddress;
+
+const requestHandled = (ipAddress) => {
+  const newIpAddressReqs = new Map([...ipAddressReqs]);
+  if (newIpAddressReqs.has(ipAddress)) {
+    let accIpAddressReqs = newIpAddressReqs.get(ipAddress);
+    accIpAddressReqs += 1;
+    newIpAddressReqs.set(ipAddress, accIpAddressReqs);
+  } else {
+    newIpAddressReqs.set(ipAddress, 1);
+  }
+  return newIpAddressReqs;
+};
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  console.log('Hola Mundo')
+  ipAddressReqs = new Map([...requestHandled(parseIp(req))]);
+  console.log(ipAddressReqs.size)
 
-  res.end(parseIp(req))
+  res.end('Finally done')
 };
 
 const server = http.createServer(requestListener);
